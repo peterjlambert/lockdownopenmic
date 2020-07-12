619153cbef2878555ce342c81e215cfebe46a9b2
Pete Lambert <pete@petelambert.com>
2020-07-12 21:16:30 +0100

    Adds minification

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
 module.exports = (config) => {
-  // Set directories to pass through to the dist folder
-  config.addPassthroughCopy("./src/images/");
+  // Only minify HTML if we are in production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
+
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
+
+  // Transforms
+  const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+
+  // Create a helpful production flag
+  const isProduction = process.env.NODE_ENV === "production";
+
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
