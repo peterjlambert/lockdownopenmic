4799c2154f1ca0d95f34e3a131e607db526de944
Pete Lambert <pete@petelambert.com>
2020-07-12 21:20:05 +0100

    Eleventy fix

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = (config) => {
   // Create a helpful production flag
   const isProduction = process.env.NODE_ENV === "production";
 
+  // Transforms
+  const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+
   // Only minify HTML if we are in production because it slows builds _right_ down
   if (isProduction) {
     config.addTransform("htmlmin", htmlMinTransform);
@@ -9,9 +12,6 @@ module.exports = (config) => {
 
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
-
-  // Transforms
-  const htmlMinTransform = require("./src/transforms/html-min-transform.js");
 
   return {
     markdownTemplateEngine: "njk",
