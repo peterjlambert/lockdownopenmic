9b267b88113e47a7afbd9c875272d5121ece4607
Pete Lambert <pete@petelambert.com>
2020-07-12 21:03:20 +0100

    Eleventy all the LockdownOpenMic

---
+++ gulp-tasks/fonts.js
@@ -0,0 +1,18 @@
+const { dest, src } = require("gulp");
+const GetGoogleFonts = require("get-google-fonts");
+
+const fonts = async () => {
+  // Setup of the library instance by setting where we want
+  // the output to go. CSS is relative to output font directory
+  const instance = new GetGoogleFonts({
+    outputDir: "./dist/fonts",
+    cssFile: "./fonts.css",
+  });
+
+  // Grabs fonts and CSS from google and puts in the dist folder
+  const result = await instance.download("https://fonts.googleapis.com/css?family=Dosis:400,500,800");
+
+  return result;
+};
+
+module.exports = fonts;
